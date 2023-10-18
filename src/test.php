27c05cf7900de0ffdf9851a0064f14afd287e7db
k21rs005 <113566783+k21rs005@users.noreply.github.com>
2023-10-18 12:58:31 +0900

    first commit

---
+++ src/test.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="js/sample.js"></script>
+<table id="form_table">
+  <tr><td><input type="text" id="inputform_0" placeholder="フォーム-0"></td></tr>
+</div>
+<input type="button" value="フォーム追加" onclick="addForm()">
+<input type="button" value="フォーム削除" onclick="removeForm()">
